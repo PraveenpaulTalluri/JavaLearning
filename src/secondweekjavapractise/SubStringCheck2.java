571550d2ec5455266f3ca73f6b7f77e2bd9fafa6
Praveen Talluri <talluri_p@India.XoriantCorp.com>
2021-06-15 17:53:15 +0530

    temp Commit

--- src/secondweekjavapractise/SubStringCheck2.java
+++ src/secondweekjavapractise/SubStringCheck2.java
@@ -1,5 +1,46 @@
 package secondweekjavapractise;
 
-public class SubStringCheck2 {
+import java.util.Scanner;
 
+public class SubStringCheck2 {
+	static void check(String[] arrayOfStrings,int noOfStrings)
+	{
+		for(int i=0;i<noOfStrings-1;i++)
+		{
+			for(int j=i+1;j<noOfStrings;j++)
+			{
+			
+				
+				  if(arrayOfStrings[j].contains(arrayOfStrings[i])) {
+				  System.out.println("["+i+","+j+"]"); }
+				  else if(arrayOfStrings[i].contains(arrayOfStrings[j])) {
+				  System.out.println("["+j+","+i+"]"); }
+				 
+			}
+			
+		}
+	}
+	
+
+	public static void main(String args[])
+	{
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.println("Praveen");
+		System.out.println("Enter number of Strings: ");
+		int noOfStrings=sc.nextInt();
+		
+		String []arrayOfStrings=new String[noOfStrings];
+		System.out.println("Enter Strings into the array: ");
+		
+		for(int i=0;i<noOfStrings;i++)
+		{
+			arrayOfStrings[i]=sc.next();
+		}
+		
+		check(arrayOfStrings,noOfStrings);
+		
+		sc.close();
+		}
+	
 }
